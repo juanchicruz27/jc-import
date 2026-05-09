af68c96107642143d8746014cdd156c74dcda3e0
JC Import <admin@jcimport.com>
2026-05-09 01:24:01 -0300

    Fix Prisma on Vercel

--- apps/landing-page/next.config.mjs
+++ apps/landing-page/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["database"],
+  serverExternalPackages: ["@prisma/client", "database"]
 };
 export default nextConfig;
